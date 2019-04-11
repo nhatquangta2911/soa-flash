411ecb0f603fbe785f2a28990f7dc5d530408600
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-11 08:22:32 +0700

    Create Project + Config

---
+++ src/main/java/com/ryan/soaproject/SoaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.soaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaProjectApplication.class, args);
+    }
+
+}
